f4e86d600236d6c4e4f0bcde67d50c1b838a47bf
tomoya4130 <m.tomoya0314@gmail.com>
2020-09-09 13:02:10 +0900

    itemsテーブルのマイグレーション設定

---
+++ db/migrate/20200909032750_create_ideas.rb
@@ -0,0 +1,11 @@
+class CreateIdeas < ActiveRecord::Migration[6.0]
+  def change
+    create_table :ideas do |t|
+      t.integer    :genre_id, null: false
+      t.string     :title,    null: false
+      t.text       :content,  null:false
+      #t.references :user,     null: false, foreign_key: true
+      t.timestamps
+    end
+  end
+end
