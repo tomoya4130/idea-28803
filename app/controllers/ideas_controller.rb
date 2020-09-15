d46cc50ae6fc908bde78720e90074b2e95672b21
tomoya4130 <m.tomoya0314@gmail.com>
2020-09-15 12:32:31 +0900

    activehash(仮)の作成

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -8,4 +8,19 @@ class IdeasController < ApplicationController
   def new
     @idea = Idea.new
   end
+
+  def create
+    @idea = Idea.new(idea_params)
+    if @idea.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def idea_params
+    params.require(:idea).permit(:genre_id, :image, :title, :content)
+  end
 end
