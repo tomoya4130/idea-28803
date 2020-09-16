6ae1dcffbb98689f667e8b541074f58344f2fa06
tomoya4130 <m.tomoya0314@gmail.com>
2020-09-16 17:28:51 +0900

    詳細ページへの遷移

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "ideas#index"
-  resources :ideas, only: [:index, :new, :create]
+  resources :ideas, only: [:index, :new, :create, :show]
 end
