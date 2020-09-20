959668caa62fd9764dfcbee74407c1b398ab1079
tomoya4130 <m.tomoya0314@gmail.com>
2020-09-20 10:10:07 +0900

    コントローラーの設定

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,10 @@
 class CommentsController < ApplicationController
   def new
+    @comments = Comment.all
+    @comment = Comment.new
+  end
+
+  def create
+    @message = Message.new(text: params[:comment][:message])
   end
 end
