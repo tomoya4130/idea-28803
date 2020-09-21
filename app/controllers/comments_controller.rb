5be758e36a78c56bcc14833005aa54e741755cdb
tomoya4130 <m.tomoya0314@gmail.com>
2020-09-21 11:48:02 +0900

    コメントフォームの表示

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,6 +5,15 @@ class CommentsController < ApplicationController
   end
 
   def create
-    @message = Message.new(text: params[:comment][:message])
+    @comment = Message.new(comment_params)
+    if @comment.save
+      ActionCable.server.broadcast 'comment_channel', content: @comment
+    end
   end
 end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:message).merge(user_id: current_user.id, idea_id: params[:idea_id])
+  end
