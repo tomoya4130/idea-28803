01651ef8738d1f0772b26a6c589a83546001eecc
tomoya4130 <m.tomoya0314@gmail.com>
2020-09-25 12:45:43 +0900

    activehashの修正

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,9 +1,11 @@
 class Genre < ActiveHash::Base
   self.data = [
     { id: 1, name: '--'},
-    { id: 2, name: '仮'},
-    { id: 3, name: '仮2'},
-    { id: 4, name: '仮3'},
-    { id: 5, name: '仮4'},
+    { id: 2, name: '生活'},
+    { id: 3, name: 'ゲーム'},
+    { id: 4, name: 'スポーツ'},
+    { id: 5, name: '本'},
+    { id: 6, name: '映画'}
+    { id: 7, name: 'その他'}
   ]
 end
